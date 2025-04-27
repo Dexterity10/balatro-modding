7e6130adbf0de1da59ac54d2af6fb319559daa7a
Dexterity <alex.goemans@gmail.com>
2025-04-27 16:48:49 -0400

    funky guys

---
+++ Goofy and Silly/FunkyGuys.lua
@@ -0,0 +1,24 @@
+SMODS.Atlas {
+    key = "funkyGuys",
+    px = 71,
+    py = 95,
+    path = 'Jokers_Funky.png',
+    prefix_config = {
+        key = false
+    }
+}
+
+SMODS.Joker {
+    key = "first_joker",
+    loc_txt = {
+        name = "El Primero Hermano",
+        text = {"this is a description of all time.", "second line"}
+    },
+    atlas = "funkyGuys",
+    pos = {
+        x = 0,
+        y = 0
+    },
+    cost = 1,
+    discovered = true
+}
