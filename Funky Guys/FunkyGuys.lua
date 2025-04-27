95ec107d66ab41b1f6fe6018111ffc9562f29307
Dexterity <alex.goemans@gmail.com>
2025-04-27 17:21:48 -0400

    funky guys atlas change

--- Funky Guys/FunkyGuys.lua
+++ Funky Guys/FunkyGuys.lua
@@ -2,23 +2,77 @@ SMODS.Atlas {
     key = "funkyGuys",
     px = 71,
     py = 95,
-    path = 'Jokers_Funky.png',
-    prefix_config = {
-        key = false
-    }
+    path = 'Jokers_Funky.png' -- 3x2 grid; 0,0 is a placeholder
 }
 
 SMODS.Joker {
-    key = "first_joker",
+    key = "firstBrother",
     loc_txt = {
         name = "El Primero Hermano",
-        text = {"this is a description of all time.", "second line"}
+        text = {"The oldest.", "+X1 mult per brother you have"}
     },
     atlas = "funkyGuys",
-    pos = {
-        x = 0,
+    pos = { -- 0,0 placeholder
+        x = 1,
         y = 0
     },
     cost = 1,
     discovered = true
 }
+SMODS.Joker {
+    key = "secondBrother",
+    loc_txt = {
+        name = "The Second Brother",
+        text = {"this is a description of all time.", "second line"}
+    },
+    atlas = "funkyGuys",
+    pos = { -- 0,0 placeholder
+        x = 2,
+        y = 0
+    },
+    cost = 1,
+    discovered = true
+}
+SMODS.Joker {
+    key = "thirdBrother",
+    loc_txt = {
+        name = "Der Dritte Bruder",
+        text = {"this is a description of all time.", "second line"}
+    },
+    atlas = "funkyGuys",
+    pos = { -- 0,0 placeholder
+        x = 0,
+        y = 1
+    },
+    cost = 1,
+    discovered = true
+}
+SMODS.Joker {
+    key = "fourthBrother",
+    loc_txt = {
+        name = "Le quatrième frère",
+        text = {"this is a description of all time.", "second line"}
+    },
+    atlas = "funkyGuys",
+    pos = { -- 0,0 placeholder
+        x = 1,
+        y = 1
+    },
+    cost = 1,
+    discovered = true
+}
+SMODS.Joker {
+    key = "fifthBrother",
+    loc_txt = {
+        name = "Den femte brodern",
+        text = {"this is a description of all time.", "second line"}
+    },
+    atlas = "funkyGuys",
+    pos = { -- 0,0 placeholder
+        x = 2,
+        y = 1
+    },
+    cost = 1,
+    discovered = true
+}
+
