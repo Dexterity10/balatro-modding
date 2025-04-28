74874bc2df3b2a7791d5bfef0ed3b67539734071
Dexterity <alex.goemans@gmail.com>
2025-04-27 22:09:26 -0400

    mouthwashing mod update - simplified the pngs (saves you a bit of space) - made the code a bit more readable too

--- mouthwashing/mouthwashing.lua
+++ mouthwashing/mouthwashing.lua
@@ -1,55 +1,22 @@
-local atlas_key = 'MW' -- Format: PREFIX_KEY
--- See end of file for notes
-local atlas_path = 'MW_lc.png' -- Filename for the image in the asset folder
--- local atlas_path_hc = 'MW_hc.png' -- Filename for the high-contrast version of the texture, if existing
-
-local suits = {'hearts'} -- Which suits to replace
-local ranks = {'King', 'Queen', "Jack"} -- Which ranks to replace
-
-local description = 'Mouthwashing' -- English-language description, also used as default
-
------------------------------------------------------------
--- You should only need to change things above this line --
------------------------------------------------------------
+local prefix = "MW"
+-- local allRanks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace"}
 
 SMODS.Atlas {
-    key = atlas_key .. '_lc',
+    key = prefix .. "_lc",
+    path = prefix .. "_lc.png",
     px = 71,
-    py = 95,
-    path = atlas_path,
-    prefix_config = {
-        key = false
-    } -- See end of file for notes
+    py = 95
 }
 
-if atlas_path_hc then
-    SMODS.Atlas {
-        key = atlas_key .. '_hc',
-        px = 71,
-        py = 95,
-        path = atlas_path_hc,
-        prefix_config = {
-            key = false
-        } -- See end of file for notes
-    }
-end
-
-for _, suit in ipairs(suits) do
-    SMODS.DeckSkin {
-        key = suit .. "_skin",
-        suit = suit:gsub("^%l", string.upper),
-        ranks = ranks,
-        lc_atlas = atlas_key .. '_lc',
-        hc_atlas = (atlas_path_hc and atlas_key .. '_hc') or atlas_key .. '_lc',
-        loc_txt = {
-            ['en-us'] = description
-        },
-        posStyle = 'deck'
-    }
-end
-
--- Notes:
-
--- The current version of Steamodded has a bug with prefixes in mods including `DeckSkin`s.
--- By manually including the prefix in the atlas' key, this should keep the mod functional
--- even after this bug is fixed.
+SMODS.DeckSkin {
+    key = "mouthwash",
+    suit = "Hearts",
+    loc_txt = "Mouthwashing",
+    palettes = {{
+        key = "lc",
+        ranks = {"Jack", "Queen", "King"},
+        display_ranks = {"King", "Queen", "Jack"},
+        atlas = prefix .. "_lc",
+        pos_style = "collab"
+    }}
+}
